6a4ee2652d22585098b318c0bdfef399a7ae74d8
jeruktutut2 <jeruktutut2@gmail.com>
2018-12-23 16:25:59 +0800

    commit awal

---
+++ src/main/java/com/leju/onetomanymanytoone/OnetomanymanytooneApplication.java
@@ -0,0 +1,14 @@
+package com.leju.onetomanymanytoone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnetomanymanytooneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnetomanymanytooneApplication.class, args);
+	}
+
+}
+
